a0ad375786b555234ebc2f67d39a37190c09696b
Uri Herrera <hello@uriherrera.com>
2020-06-27 00:44:37 -0500

    update files

--- travis-build.sh
+++ travis-build.sh
@@ -7,7 +7,7 @@ apt-get --yes install wget gnupg2
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 55751E5D
 wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/master/configs/files/sources.list.neon.user
 
-### Add Bionic Repository.
+### Add Bionic Repository
 wget -qO /etc/apt/sources.list.d/ubuntu-bionic-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/master/configs/files/sources.list.bionic
 
 ### Install Dependencies
