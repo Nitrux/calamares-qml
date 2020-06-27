3429062bbd67bae6dfd1f7064071b3c297c8b701
Uri Herrera <hello@uriherrera.com>
2020-06-27 00:36:13 -0500

    Update travis-build.sh

--- travis-build.sh
+++ travis-build.sh
@@ -8,6 +8,7 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 55751E5D
 wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/master/configs/files/sources.list.neon.user
 
 ### Add Bionic Repository.
+wget -qO /etc/apt/sources.list.d/ubuntu-bionic-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/master/configs/files/sources.list.bionic
 
 ### Install Dependencies
 apt-get --yes update
