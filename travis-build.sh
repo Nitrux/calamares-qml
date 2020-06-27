3be6277656ec219e700ec3785fb35591849393b3
Uri Herrera <hello@uriherrera.com>
2020-06-27 02:43:00 -0500

    ...

--- travis-build.sh
+++ travis-build.sh
@@ -8,7 +8,7 @@ apt-get --yes install wget gnupg2
 ### Install Dependencies
 apt-get --yes update
 apt-get --yes dist-upgrade
-DEBIAN_FRONTEND=noninteractive apt-get --yes install devscripts lintian build-essential automake autotools-dev equivs keyboard-configuration console-setup qttools5-dev libyaml-cpp-dev libpolkit-qt5-1-dev libkf5crash-dev python3-dev libkpmcore-dev libkf5parts-dev libappstreamqt-dev libkf5plasma-dev libkf5package-dev libpwquality-dev libparted-dev libboost-dev libboost-python-dev
+DEBIAN_FRONTEND=noninteractive apt-get --yes install devscripts lintian build-essential automake autotools-dev equivs keyboard-configuration console-setup
 mk-build-deps -i -t "apt-get --yes" -r
 
 ### Build Deb
