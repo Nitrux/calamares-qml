3cb01ea34d0496581b7e96aae9277e67769ebabd
Uri Herrera <hello@uriherrera.com>
2020-06-27 19:06:50 -0500

    ....

--- travis-build.sh
+++ travis-build.sh
@@ -5,15 +5,6 @@ set -x
 apt-get --yes update
 apt-get --yes install wget gnupg2
 
-### Add Focal Repository to update packages
-wget -qO /etc/apt/sources.list.d/ubuntu-focal-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/master/configs/files/sources.list.focal
-apt-get --yes update
-apt-get --yes install libyaml-cpp-dev libpython3.8-dev libboost-python1.71-dev
-
-### Remove Focal Repository
-
-rm /etc/apt/sources.list.d/ubuntu-focal-repo.list 
-
 ### Install Dependencies
 apt-get --yes update
 apt-get --yes dist-upgrade
