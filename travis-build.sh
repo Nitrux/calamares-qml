586f61a06d75bab73e5d65dffb1a4d9112bd155d
Uri Herrera <hello@uriherrera.com>
2020-08-14 14:54:16 -0500

    [ci skip]

--- travis-build.sh
+++ travis-build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-apt update
-apt -qq -yy install curl git
+apt -qq update
+apt -qq -yy install equivs curl git
 
 ### Install Dependencies
 apt -qq -yy dist-upgrade
-DEBIAN_FRONTEND=noninteractive apt -qq -yy install devscripts lintian build-essential automake autotools-dev equivs keyboard-configuration console-setup
+DEBIAN_FRONTEND=noninteractive apt -qq -yy install devscripts lintian build-essential automake autotools-dev keyboard-configuration console-setup
 mk-build-deps -i -t "apt-get --yes" -r
 
 ### Build Deb
