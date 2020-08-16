2696275b93a0406213df7b5b2fb456883bdee34a
Uri Herrera <hello@uriherrera.com>
2020-08-15 19:24:28 -0500

    [ci skip]

--- travis-build.sh
+++ travis-build.sh
@@ -3,7 +3,7 @@
 apt -qq update
 apt -qq -yy install equivs curl git wget gnupg2
 
-### FIXME - the container does have the neon repo but for some idiotic reason it isn't working here
+### FIXME - the container mauikit/ubuntu-18.04-amd64 does have the neon repo but for some idiotic reason it isn't working here
 
 wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/master/configs/files/sources.list.neon.user
 
