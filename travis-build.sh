dcdd4f5397282b0d5f9711aaf4368cb1a027506a
Uri Herrera <hello@uriherrera.com>
2020-08-15 19:12:11 -0500

    srsly

--- travis-build.sh
+++ travis-build.sh
@@ -5,7 +5,7 @@ apt -qq -yy install equivs curl git wget gnupg2
 
 ### FIXME - the container does have the neon repo but for some idiotic reason it isn't working here
 
-wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://github.com/Nitrux/iso-tool/blob/master/configs/files/sources.list.neon.user
+wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/master/configs/files/sources.list.neon.user
 
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
 	55751E5D > /dev/null
