91ad79b0030265ffb3b95ce9f7390046450dd45a
Uri Herrera <hello@uriherrera.com>
2020-10-25 03:23:28 -0600

    use focal to build pkg

--- travis-build.sh
+++ travis-build.sh
@@ -5,7 +5,7 @@ apt -qq -yy install equivs curl git wget gnupg2
 
 ### FIXME - the container mauikit/ubuntu-18.04-amd64 does have the neon repo but for some idiotic reason it isn't working here
 
-wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/master/configs/files/sources.list.neon.user
+wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.neon.user
 
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
 	55751E5D > /dev/null
